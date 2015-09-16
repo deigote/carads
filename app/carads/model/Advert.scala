c2d11a6e7dd23fb515a7f4b856b3a0d440558f1d
deigote <diego@toharia.com>
2015-09-16 11:02:39 +0200

    advert allow to get a copy with a concrete id
    Since Advert id is optional (to allow non persisted adverts to be represented), a method to get a copy (to respect immutability) with an id is added. This method can be used after insertion to set the id in the inserted Advert

--- app/carads/model/Advert.scala
+++ app/carads/model/Advert.scala
@@ -7,16 +7,27 @@ abstract class Advert(id: Option[Int],
                       fuel: Fuel,
                       price: Int,
                       mileage: Option[Int],
-                      firstRegistration: Option[LocalDate])
+                      firstRegistration: Option[LocalDate]) {
+  def withId(id: Int): Advert
+  def getId(): Option[Int] = id
+}
 
 case class AdvertForUsed(id: Option[Int],
                          title: String,
                          fuel: Fuel,
                          price: Int,
                          mileage: Int,
-                         firstRegistration: LocalDate) extends Advert(id, title, fuel, price, Some(mileage), Some(firstRegistration))
+                         firstRegistration: LocalDate) extends Advert(id, title, fuel, price, Some(mileage), Some(firstRegistration)) {
+
+  override def withId(id: Int): Advert =
+    AdvertForUsed(Some(id), title, fuel, price, mileage, firstRegistration)
+
+}
 
 case class AdvertForNew(id: Option[Int],
                         title: String,
                         fuel: Fuel,
-                        price: Int) extends Advert(id, title, fuel, price, None, None)
+                        price: Int) extends Advert(id, title, fuel, price, None, None) {
+  override def withId(id: Int): Advert =
+    AdvertForNew(Some(id), title, fuel, price)
+}
