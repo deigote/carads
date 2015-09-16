cce2c69deb282e98deed7ad2b7fa5a66859a37f0
deigote <diego@toharia.com>
2015-09-16 13:51:19 +0200

    adverts repo allows creation from a generic advert
    Again, it seems that abstract class + extended case classes is not the best choice. Hard to refactor without better Scala knowledge! for the time being, used case + asInstanceOf for consistency.

--- app/carads/services/AdvertsRepository.scala
+++ app/carads/services/AdvertsRepository.scala
@@ -11,12 +11,17 @@ import com.google.inject.ImplementedBy
 @ImplementedBy(classOf[AdvertsJdbcRepository])
 trait AdvertsRepository {
 
+
   def list(): List[Advert] = list(None)
   def list(sortBy: Option[String]): List[Advert] = list(sortBy.getOrElse("id"))
   def list(sortBy: String): List[Advert]
 
   def clear(): Unit
 
+  def create(advert: Advert): Advert = advert match {
+    case AdvertForNew(_,_,_,_) => create(advert.asInstanceOf[AdvertForNew])
+    case AdvertForUsed(_,_,_,_,_,_) => create(advert.asInstanceOf[AdvertForUsed])
+  }
   def create(advert: AdvertForNew): Advert
   def create(advert: AdvertForUsed): Advert
 
