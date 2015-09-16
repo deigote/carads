ec38ee364880b1638cf70f0f4851510f4f71be7b
deigote <diego@toharia.com>
2015-09-16 13:15:25 +0200

    Advert provides getters for its fields
    Model should probably be rethought, but for that Scala must be better investigated. Seems that abstract class + case classes extending it is not a great approach. Maybe case classes that inherit from others should be restricted for pure algebraic types? For the time being, getters are added until further investigation.

--- app/carads/model/Advert.scala
+++ app/carads/model/Advert.scala
@@ -2,14 +2,21 @@ package carads.model
 
 import java.time.LocalDate
 
+// TODO: investigate Scala more and rethink model - no auto-getters for constructor params in abstract classes!
 abstract class Advert(id: Option[Int],
                       title: String,
                       fuel: Fuel,
                       price: Int,
                       mileage: Option[Int],
                       firstRegistration: Option[LocalDate]) {
+
   def withId(id: Int): Advert
   def getId(): Option[Int] = id
+  def getTitle(): String = title
+  def getFuel(): Fuel = fuel
+  def getPrice(): Int = price
+  def getMileage(): Option[Int] = mileage
+  def getFirstRegistration(): Option[LocalDate] = firstRegistration
 }
 
 case class AdvertForUsed(id: Option[Int],
@@ -19,7 +26,7 @@ case class AdvertForUsed(id: Option[Int],
                          mileage: Int,
                          firstRegistration: LocalDate) extends Advert(id, title, fuel, price, Some(mileage), Some(firstRegistration)) {
 
-  override def withId(id: Int): Advert =
+  override def withId(id: Int): AdvertForUsed =
     AdvertForUsed(Some(id), title, fuel, price, mileage, firstRegistration)
 
 }
@@ -28,6 +35,6 @@ case class AdvertForNew(id: Option[Int],
                         title: String,
                         fuel: Fuel,
                         price: Int) extends Advert(id, title, fuel, price, None, None) {
-  override def withId(id: Int): Advert =
+  override def withId(id: Int): AdvertForNew =
     AdvertForNew(Some(id), title, fuel, price)
 }
